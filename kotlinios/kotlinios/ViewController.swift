b2055f1968d0b389d58700fa463991dbe77ccb09
Yasuhiro Shimizu <the.phantom.bane@gmail.com>
2018-10-26 00:10:02 +0900

    add ios skelton

---
+++ kotlinios/kotlinios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kotlinios
+//
+//  Created by Yasuhiro Shimizu on 2018/10/26.
+//  Copyright © 2018年 CodingFeline. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
